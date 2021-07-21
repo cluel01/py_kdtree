b454c0fffd42f1589cc5191cf27922338ddc04cd
cluel01 <c.luelf96@gmx.de>
2021-07-21 13:31:02 +0200

    updated test.py

--- tests/test.py
+++ tests/test.py
@@ -2,12 +2,11 @@ import numpy as np
 from py_kdtree.kdtree import KDTree
 np.random.seed(42)
 
-X = np.random.randn(10000,3)
+X = np.random.randn(100000,3)
 
-tree = KDTree(leaf_size=1000,path="/home/cluelf/py_kdtree/run")
+tree = KDTree(leaf_size=333,path="/home/cluelf/py_kdtree/run")
 
-if tree._root is None:
-    tree.fit(X)
+tree.fit(X)
 
 inds,pts = tree.query_box(np.array([0,0,0]),np.array([0.5,.1,.1]))
 print(len(inds))
