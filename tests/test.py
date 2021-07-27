1ee657b8e067738cc551f29a03ed741f6bc58480
cluel01 <c.luelf96@gmx.de>
2021-07-27 10:34:22 +0200

    test update

--- tests/test.py
+++ tests/test.py
@@ -4,7 +4,7 @@ np.random.seed(42)
 
 X = np.random.randn(10000000,3).astype(np.float32)
 
-tree = KDTree(leaf_size=10000,path="/home/cluelf/py_kdtree/run",dtype="float32")
+tree = KDTree(leaf_size=20000,path="/home/cluelf/py_kdtree/run",dtype="float32")
 
 tree.fit(X)
 
