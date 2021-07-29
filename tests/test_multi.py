00eafacf3ac8dcd6e1b1e58139c5930f3b2c9e54
cluel01 <c.luelf96@gmx.de>
2021-07-29 13:19:13 +0200

    minor

--- tests/test_multi.py
+++ tests/test_multi.py
@@ -9,12 +9,12 @@ X = np.random.randn(100000,7).astype(np.float32)
 idxs = [[0,1,2],[3,4,5,6],[0,1,3]]
 
 
-ens = KDTreeSet(idxs,leaf_size=20,path="/home/cluelf/py_kdtree/run",dtype="float32",verbose=False)
+ens = KDTreeSet(idxs,leaf_size=20,path="/home/cluelf/py_kdtree/run",dtype="float32",verbose=True)
 
 ens.fit(X)
 
 start = time.time()
-inds,pts = ens.multi_query(np.array([[0,0,0],[0,0,0]]),np.array([[0.5,0.5,0.1],[0.1,0.1,0.2]]),[idxs[i] for i in [0,2]],n_jobs=1)
+inds,pts = ens.multi_query(np.array([[0,0,0],[0,0,0]]),np.array([[0.5,0.5,0.1],[0.1,0.1,0.2]]),[idxs[i] for i in [0,2]],n_jobs=-1)
 end = time.time()
 print(len(inds))
 print(end-start)
