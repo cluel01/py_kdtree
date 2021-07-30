dc6cb294cc6f8d4f22423a7d8a0f12c2e77ece72
cluel01 <c.luelf96@gmx.de>
2021-07-30 11:45:54 +0200

    test fit seq

---
+++ tests/test_fit_seq.py
@@ -0,0 +1,31 @@
+import numpy as np 
+from py_kdtree.kdtree import KDTree
+from py_kdtree.treeset import KDTreeSet
+np.random.seed(42)
+import os
+
+x1 = np.random.randn(100000,7).astype(dtype="float32")
+x2 = np.random.randn(200000,7).astype(dtype="float32")
+np.save("/home/cluelf/py_kdtree/run/x1.npy",x1)
+np.save("/home/cluelf/py_kdtree/run/x2.npy",x2)
+
+x_files = [i for i in os.listdir("/home/cluelf/py_kdtree/run/") if i.endswith(".npy")]
+
+idxs = [[0,1,2],[3,4,5,6],[0,1,3]]
+
+
+ens = KDTreeSet(idxs,leaf_size=20,path="/home/cluelf/py_kdtree/run",dtype="float32")
+
+ens.fit_seq(x_files,n_chached=2)
+#ens.fit(np.vstack([x1,x2]))
+
+
+inds,pts = ens.query(np.array([0,0,0]),np.array([0.5,.1,.1]),idxs[0])
+print(len(inds))
+print(len(pts))
+#print(inds)
+
+inds,pts = ens.query(np.array([0,0,0,0]),np.array([0.5,0.1,0.1,0.1]),idxs[1])
+
+print(len(inds))
+print(len(pts))
